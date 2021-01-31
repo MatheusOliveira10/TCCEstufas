18f63b974c406c673b938abc05ec099f403a4996
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-01-31 11:05:23 -0300

    Primeiro componente, gráfico de barras

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 import { Checkbox, Container, Divider, Grid, Header, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, LineSeries } from 'react-vis'
 import './customStyles.css'
+import LineChart from './components/LineChart'
 
 const App = () => {
   const [visible, setVisible] = useState(true)
+
+  const data = [
+    { time: 0, value: 8 },
+    { time: 1, value: 5 },
+    { time: 2, value: 4 },
+    { time: 3, value: 9 },
+    { time: 4, value: 1 },
+    { time: 5, value: 7 },
+    { time: 6, value: 6 },
+    { time: 7, value: 3 },
+    { time: 8, value: 2 },
+    { time: 9, value: 0 }
+  ];
 
   return (
     <Sidebar.Pushable>
@@ -33,7 +48,6 @@ const App = () => {
 
       <Sidebar.Pusher style={{ width: '100vw', height: '100vh' }}>
         <Container>
-
           <Header style={{ marginTop: 20 }} as='h2' icon inverted textAlign='center'>
             <Icon name='info' />
               Monitoramento da Estufa
@@ -41,13 +55,20 @@ const App = () => {
           <Divider />
 
           <Grid stackable>
-            {/* Heads up! Grid.Row is not mandatory, Grid.Column is enough for grid to work */}
             <Grid.Row columns='equal'>
-              <Grid.Column>
-                <p />
+              <Grid.Column style={{ alignItems: 'center', backgroundColor: 'white', borderRadius: 10, padding: 20 }}>
+                <LineChart
+                  data={data}
+                  title={'Teste'}
+                  color="#3E517A"
+                />
               </Grid.Column>
-              <Grid.Column>
-                <p />
+              <Grid.Column style={{ alignItems: 'center', backgroundColor: 'white', borderRadius: 10, padding: 20, marginLeft: 10 }}>
+                <LineChart
+                  data={data}
+                  title={'Teste2'}
+                  color="#3E517A"
+                />
               </Grid.Column>
             </Grid.Row>
           </Grid>
