f0eb10a5f8953c822bee7cefa9b61d536bb0c0ef
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-11-23 16:32:43 -0300

    Headers para previnir erros de CORS

--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");   
+header("Content-Type: application/json; charset=UTF-8");    
+header("Access-Control-Allow-Methods: POST, PUT, DELETE, OPTIONS");    
+header("Access-Control-Max-Age: 3600");    
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+http_response_code(200);
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
+  return 0;    
+}   
 
 require __DIR__ . '/vendor/autoload.php';
 
