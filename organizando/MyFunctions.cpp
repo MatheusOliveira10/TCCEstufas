13130946457ac4c02ff78f40990f029406ede83b
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-11-29 19:44:03 -0300

    Retirando arquivos desnecessários

--- organizando/MyFunctions.cpp
+++
@@ -1,8 +0,0 @@
-#include "MyFunctions.h"
-#include "Arduino.h"
-
-void MyFunctions::sendData(const char *topic, const char *message, PubSubClient mqttClient) {
-  if (mqttClient.connect("mobg")) {
-      mqttClient.publish(topic, message);
-  }
-}
