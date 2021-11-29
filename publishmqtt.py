2042119ff56a0a0718d8f438e515aa29ebad7b72
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-11-29 19:42:22 -0300

    simulação de envio de mensagem mqtt

--- publishmqtt.py
+++ publishmqtt.py
@@ -1,13 +1,13 @@
 import paho.mqtt.client as paho
 
-broker="test.mosquitto.org"
+broker="broker.hivemq.com"
 port=1883
 
-def on_publish(client,userdata,result):             #create function for callback
+def on_publish(client,userdata,result):             
     print("data published \n")
     pass
 
-client1= paho.Client("ClienteMobg") #create client object
-client1.on_publish = on_publish  #assign function to callback
-client1.connect(broker,port)     #establish connection
-print(client1.publish("mobg/1", 6))            #publish
+client1= paho.Client("mobg") 
+client1.on_publish = on_publish  
+client1.connect(broker,port)     
+print(client1.publish("mobg/2", 35, qos=2)) #simulando envio de mensagem do sensor 2
